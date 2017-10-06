111c1cbd135cade6384469141f3216a61563bbf1
Xericore <xericore@xericore.com>
2017-10-06 15:46:20 +0200

    Increases PlayerInteractEvent priority from LOWEST to LOW.

--- src/com/github/xericore/easycarts/PlayerClickListener.java
+++ src/com/github/xericore/easycarts/PlayerClickListener.java
@@ -10,36 +10,43 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.util.Vector;
 
-public class PlayerClickListener implements Listener {
+public class PlayerClickListener implements Listener
+{
 
 	public static EasyCarts easyCartsPlugin;
 	private static FileConfiguration config = null;
 
-	public PlayerClickListener(EasyCarts theInstance) {
+	public PlayerClickListener(EasyCarts theInstance)
+	{
 		easyCartsPlugin = theInstance;
 		config = easyCartsPlugin.getConfig();
 	}
 
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onMyPlayerInteract(PlayerInteractEvent event) {
+	@EventHandler(priority = EventPriority.LOW)
+	public void onMyPlayerInteract(PlayerInteractEvent event)
+	{
 
 		if (!config.getBoolean("StopStartOnLeftClick"))
 			return;
 
 		Player player = (Player) event.getPlayer();
 
-		if (!player.isInsideVehicle()) {
+		if (!player.isInsideVehicle())
+		{
 			return;
 		}
 
 		Vehicle cart = (Vehicle) player.getVehicle();
 
-		if (event.getAction() == Action.LEFT_CLICK_AIR) {
+		if (event.getAction() == Action.LEFT_CLICK_AIR)
+		{
 
-			if (cart.getVelocity().length() <= 0) {
-				cart.setVelocity(Utils.getUnitVectorFromYaw(player.getLocation().getYaw()).multiply(
-						(Utils.MINECART_VANILLA_PUSH_SPEED * config.getDouble("MaxPushSpeedPercent") / 100)));
-			} else {
+			if (cart.getVelocity().length() <= 0)
+			{
+				cart.setVelocity(Utils.getUnitVectorFromYaw(player.getLocation().getYaw())
+						.multiply((Utils.MINECART_VANILLA_PUSH_SPEED * config.getDouble("MaxPushSpeedPercent") / 100)));
+			} else
+			{
 				cart.setVelocity(new Vector(0, 0, 0));
 			}
 		}
