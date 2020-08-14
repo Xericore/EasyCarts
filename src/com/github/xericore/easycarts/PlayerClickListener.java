ae503699ad610c6c5fa39512ab7033fcb80d3942
Mario Petričević <mario@petricevic.net>
2020-08-14 18:16:27 +0200

    Fixes ClassCastException in PlayerClickListener.

--- src/com/github/xericore/easycarts/PlayerClickListener.java
+++ src/com/github/xericore/easycarts/PlayerClickListener.java
@@ -4,7 +4,7 @@ import com.github.xericore.easycarts.utilities.CartSpeed;
 import com.github.xericore.easycarts.utilities.Utils;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Vehicle;
+import org.bukkit.entity.minecart.RideableMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -29,26 +29,27 @@ public class PlayerClickListener implements Listener
 		if (!config.getBoolean("StopStartOnLeftClick"))
 			return;
 
-		Player player = (Player) event.getPlayer();
+		Player player = event.getPlayer();
 
 		if (!player.isInsideVehicle())
-		{
 			return;
-		}
 
-		Vehicle cart = (Vehicle) player.getVehicle();
+		if (event.getAction() != Action.LEFT_CLICK_AIR)
+			return;
 
-		if (event.getAction() == Action.LEFT_CLICK_AIR)
+		if (!(player.getVehicle() instanceof RideableMinecart))
+			return;
+
+		RideableMinecart cart = (RideableMinecart) player.getVehicle();
+
+		if (cart.getVelocity().length() <= 0)
 		{
-
-			if (cart.getVelocity().length() <= 0)
-			{
-				cart.setVelocity(Utils.getStraightUnitVectorFromYaw(player.getLocation().getYaw())
-						.multiply((CartSpeed.MINECART_VANILLA_PUSH_SPEED * config.getDouble("MaxPushSpeedPercent") / 100)));
-			} else
-			{
-				cart.setVelocity(new Vector(0, 0, 0));
-			}
+			cart.setVelocity(Utils.getStraightUnitVectorFromYaw(player.getLocation().getYaw())
+					.multiply((CartSpeed.MINECART_VANILLA_PUSH_SPEED * config.getDouble("MaxPushSpeedPercent") / 100)));
+		}
+		else
+		{
+			cart.setVelocity(new Vector(0, 0, 0));
 		}
 	}
 }
